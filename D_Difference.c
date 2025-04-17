b00e529273e253bc21e1abb9b9371ce2a9d4552b
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-17 17:59:03 +0600

    diffrence

---
+++ D_Difference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a, c;
+    long long int b, d;
+    scanf("%d %lld %d %lld", &a, &b, &c, &d);
+    
+    printf("Difference = %lld", a*b - c*d);
+
+    return 0;
+}
